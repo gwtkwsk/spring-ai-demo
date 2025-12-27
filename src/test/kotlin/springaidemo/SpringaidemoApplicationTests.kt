e66bffb27b8162d2295e4f16f3d443d081fd5d30
Grzegorz Witkowski <gmwitkowski@gmail.com>
2025-12-27 15:37:59 +0100

    Init spring boot project

---
+++ src/test/kotlin/springaidemo/SpringaidemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package springaidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringaidemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
