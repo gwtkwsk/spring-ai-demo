335c1026322390392e0c45e84fe18d7413e4a593
Grzegorz Witkowski <gmwitkowski@gmail.com>
2025-12-30 17:12:31 +0100

    Simple chat with memory

---
+++ src/main/java/springaidemo/MyController.java
@@ -0,0 +1,33 @@
+package springaidemo;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+@RestController
+class MyController {
+
+    private final ChatClient chatClient;
+
+    public MyController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        this.chatClient = chatClientBuilder.clone()
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+    }
+
+    @PostMapping("/chat/{conversationId}")
+    public String chat(@PathVariable String conversationId, @RequestBody String question) {
+        return chatClient
+                .prompt()
+                .user(question)
+                .advisors(a -> a.param(CONVERSATION_ID, conversationId))
+                .call()
+                .content();
+    }
+}
---
+++ src/main/java/springaidemo/SpringaidemoApplication.java
@@ -0,0 +1,12 @@
+package springaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaidemoApplication.class, args);
+    }
+}
